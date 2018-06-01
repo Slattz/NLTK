74d10c1aadfadcb09ae9bc7f974177894137ecdd
Slattz <Slattz@users.noreply.github.com>
2018-06-01 02:42:09 +0100

    Add extra checksum to ensure all checksums are updated

--- include/checksum.h
+++ include/checksum.h
@@ -6,9 +6,16 @@
 
 //class Save;
 
-u32 CalculateCRC32(u8 *buf, u32 size);
-bool VerifyCRC32(u32 crc, u8 *buf, u32 startOffset, u32 size);
-u32 UpdateCRC32(Save *save, u32 startOffset, u32 size);
+typedef enum
+{
+    CHECKSUM_1 = 0,
+    CHECKSUM_2 = 1
+} ChecksumType;
+
+u32 CalculateCRC32Type1(u8 *buf, u32 size);
+u32 CalculateCRC32Type2(u8 *buf, u32 size);
+bool VerifyCRC32(u32 crc, u8 *buf, u32 startOffset, u32 size, ChecksumType type = CHECKSUM_1);
+u32 UpdateCRC32(Save *save, u32 startOffset, u32 size, ChecksumType type = CHECKSUM_1);
 void FixCRC32s(Save *save);
 
 #endif
