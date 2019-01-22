f27afe5ed2627dcc692ab3e5f1795fae3012e4e6
Slattz <Slattz@users.noreply.github.com>
2019-01-22 22:36:05 +0000

    New version number, yay

--- include/common.h
+++ include/common.h
@@ -7,8 +7,8 @@
 #define TOSTRING(x)    STRINGIFY(x)
 
 #define MAJOR_VERSION  0
-#define MINOR_VERSION  2
-#define REV_VERSION    7
+#define MINOR_VERSION  3
+#define REV_VERSION    0
 #define BETA_VERSION   0
 #define VERSION        "v" TOSTRING(MAJOR_VERSION) "." TOSTRING(MINOR_VERSION) "." TOSTRING(REV_VERSION)
 #define WORKDIR        "sdmc:/NLTK"
