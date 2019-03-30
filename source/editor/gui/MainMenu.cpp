c8cfe299972afff0175df5dffa0b445ff03eeb04
Slattz <Slattz@users.noreply.github.com>
2019-03-30 19:15:20 +0000

    [Editor] Main Menu - Add a cleanup function give a proper namespace update to new player array save stuff

--- source/editor/gui/MainMenu.cpp
+++ source/editor/gui/MainMenu.cpp
@@ -32,19 +32,18 @@ ImageButton *OptionsButton = nullptr;
 ImageButton *GameSelectButton = nullptr;
 ImageButton *TownManagerButton = nullptr;
 
-static const float TextSize = 0.6f;
+static constexpr float TextSize = 0.6f;
 
-static int Initialized;
-static std::vector<Text> ButtonText;
-static std::vector<Text> ColumnText;
+static std::vector<Text *> ButtonText;
+static std::vector<Text *> ColumnText;
 static int ButtonIcon[] = {TOWN_ICON, ACRES_ICON, PROFILE_ICON, VILLAGERS_ICON,
                                PWP_ICON, ISLAND_ICON, SHOPS_ICON, ABOUT_ICON};
 
-bool Editor::SaveCheck(void) {
+bool Editor::MainMenu::SaveCheck(void) {
     return MsgDisp(top, "Would you like to save changes\nmade to your save?", MsgTypeConfirm);
 }
 
-void Editor::Init_MainMenu(void) {
+void Editor::MainMenu::Initialize(void) {
     TownButton = new ImageButton(15.f, 110.f, 70.f, 55.f, 0, KEY_A | KEY_TOUCH, BUTTON_MAIN, Editor_ss);
     AcreButton = new ImageButton(89.f, 110.f, 70.f, 55.f, 0, KEY_A | KEY_TOUCH, BUTTON_MAIN, Editor_ss);
     PlayerButton = new ImageButton(163.f, 110.f, 70.f, 55.f, 0, KEY_A | KEY_TOUCH, BUTTON_MAIN, Editor_ss);
@@ -64,24 +63,60 @@ void Editor::Init_MainMenu(void) {
     GameSelectButton->SetScale(1.15f, 0.6f);
     TownManagerButton->SetScale(1.15f, 0.6f);
 
-    ButtonText.push_back(Text(COLOR_GREY, "Town", TextSize, TextSize, 37.f, 147.f));
-    ButtonText.push_back(Text(COLOR_GREY, "Acres", TextSize, TextSize, 110.f, 147.f));
-    ButtonText.push_back(Text(COLOR_GREY, "Players", TextSize, TextSize, 178.f, 147.f));
-    ButtonText.push_back(Text(COLOR_GREY, "Villagers", TextSize, TextSize, 248.f, 147.f));
-    ButtonText.push_back(Text(COLOR_GREY, "PWPs", TextSize, TextSize, 37.f, 217.f));
-    ButtonText.push_back(Text(COLOR_GREY, "Island", TextSize, TextSize, 110.f, 217.f));
-    ButtonText.push_back(Text(COLOR_GREY, "Shops", TextSize, TextSize, 182.f, 217.f));
-    ButtonText.push_back(Text(COLOR_GREY, "About", TextSize, TextSize, 256.f, 217.f));
+    ButtonText.push_back(new Text(COLOR_GREY, "Town", TextSize, TextSize, 37.f, 147.f));
+    ButtonText.push_back(new Text(COLOR_GREY, "Acres", TextSize, TextSize, 110.f, 147.f));
+    ButtonText.push_back(new Text(COLOR_GREY, "Players", TextSize, TextSize, 178.f, 147.f));
+    ButtonText.push_back(new Text(COLOR_GREY, "Villagers", TextSize, TextSize, 248.f, 147.f));
+    ButtonText.push_back(new Text(COLOR_GREY, "PWPs", TextSize, TextSize, 37.f, 217.f));
+    ButtonText.push_back(new Text(COLOR_GREY, "Island", TextSize, TextSize, 110.f, 217.f));
+    ButtonText.push_back(new Text(COLOR_GREY, "Shops", TextSize, TextSize, 182.f, 217.f));
+    ButtonText.push_back(new Text(COLOR_GREY, "About", TextSize, TextSize, 256.f, 217.f));
 
-    ColumnText.push_back(Text(COLOR_GREY, "Game\nSelect", TextSize, TextSize, 44.f, 12.f));
-    ColumnText.push_back(Text(COLOR_GREY, "  Town\nManager", TextSize, TextSize, 40.f, 60.f));
-    ColumnText.push_back(Text(COLOR_GREY, "Save", TextSize, TextSize, 248.f, 18.f));
-    ColumnText.push_back(Text(COLOR_GREY, "Options", TextSize, TextSize, 240.f, 67.f));
-
-    Initialized = true;
+    ColumnText.push_back(new Text(COLOR_GREY, "Game\nSelect", TextSize, TextSize, 44.f, 12.f));
+    ColumnText.push_back(new Text(COLOR_GREY, "  Town\nManager", TextSize, TextSize, 40.f, 60.f));
+    ColumnText.push_back(new Text(COLOR_GREY, "Save", TextSize, TextSize, 248.f, 18.f));
+    ColumnText.push_back(new Text(COLOR_GREY, "Options", TextSize, TextSize, 240.f, 67.f));
 }
 
-void Editor::Draw_MainMenu(void) {
+void Editor::MainMenu::Cleanup(void) {
+    delete TownButton;
+    delete AcreButton;
+    delete PlayerButton;
+    delete VillagerButton;
+    delete PWPButton;
+    delete IslandButton;
+    delete MainStreetButton;
+    delete AboutButton;
+    delete SaveButton;
+    delete OptionsButton;
+    delete GameSelectButton;
+    delete TownManagerButton;
+    TownButton = nullptr;
+    AcreButton = nullptr;
+    PlayerButton = nullptr;
+    VillagerButton = nullptr;
+    PWPButton = nullptr;
+    IslandButton = nullptr;
+    MainStreetButton = nullptr;
+    AboutButton = nullptr;
+    SaveButton = nullptr;
+    OptionsButton = nullptr;
+    GameSelectButton = nullptr;
+    TownManagerButton = nullptr;
+
+    for (const auto button : ButtonText) {
+        delete button;
+    }
+
+    for (const auto column : ColumnText) {
+        delete column;
+    }
+
+    ButtonText.clear();
+    ColumnText.clear();
+}
+
+void Editor::MainMenu::Draw(void) {
     draw_base_interface();
     C2D_SceneBegin(bottom);
     DrawSprite(Common_ss, NLTK_ICON, 126, 10); //NLTK's Icon
@@ -94,8 +129,8 @@ void Editor::Draw_MainMenu(void) {
 
     for (int i = 0; i < 2; i++)
     {
-        ColumnText[i].Draw(); //Column 1 Text
-        ColumnText[2+i].Draw(); //Column 2 Text
+        ColumnText[i]->Draw(); //Column 1 Text
+        ColumnText[2+i]->Draw(); //Column 2 Text
     }
 
     // Draw sub buttons
@@ -112,8 +147,8 @@ void Editor::Draw_MainMenu(void) {
     {
         DrawSprite(Editor_ss, ButtonIcon[i], 39+(74*i), 114, GreenFilter); //Row 1 Icons
         DrawSprite(Editor_ss, ButtonIcon[4+i], 39+(74*i), 184, GreenFilter); //Row 2 Icons
-        ButtonText[i].Draw(); //Row 1 Text
-        ButtonText[i+4].Draw(); //Row 2 Text
+        ButtonText[i]->Draw(); //Row 1 Text
+        ButtonText[i+4]->Draw(); //Row 2 Text
     }
 
     C2D_SceneBegin(top);
@@ -123,15 +158,13 @@ void Editor::Draw_MainMenu(void) {
 
 ReturnMode Editor::Spawn_MainMenu()
 {
-    if (!Initialized) {
-        Init_MainMenu();
-    }
+    Editor::MainMenu::Initialize();
 
     while (aptMainLoop())
     {
         checkIfCardInserted();
 
-        Editor::Draw_MainMenu();
+        Editor::MainMenu::Draw();
         InputManager::Instance()->RefreshInput();
 
         if (InputManager::Instance()->IsButtonDown(KEY_START))
@@ -181,7 +214,7 @@ ReturnMode Editor::Spawn_MainMenu()
 
         else if (SaveButton->IsActive()) //Save Column
         {
-            if (SaveCheck())
+            if (Editor::MainMenu::SaveCheck())
             {
                 if (Save::Instance()->Commit(false))
                     MsgDisp(top, "Save Succeeded!");
@@ -190,5 +223,6 @@ ReturnMode Editor::Spawn_MainMenu()
             }
         }
     }
+    Editor::MainMenu::Cleanup();
     return ReturnMode::Exit;
 }
